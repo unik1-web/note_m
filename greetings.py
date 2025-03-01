635025b25394b7b7b293be7687c9e600640f048a
unik1-web <75004740+unik1-web@users.noreply.github.com>
2025-03-01 15:31:39 +0500

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -1,1 +1,12 @@
-
+username = 'Ivanov'
+title = 'Zametka'
+content = 'Kupit tetrad'
+status = 'в работе'
+created_date = "10-11-2024"
+issue_date = "10-12-2024"
+print('Имя пользователя', username)
+print('Заголовок заметки', title)
+print('Описание заметки', content)
+print('Статус заметки', status)
+print('Дата создания заметки', created_date)
+print('Дата истечения заметки', issue_date)
