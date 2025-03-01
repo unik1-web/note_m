07a25004269a046154ec8690f767bdf14b2d11f4
unik-web <unin.konstantin@yandex.ru>
2025-03-01 15:57:03 +0500

    Использование словаря для данных заметки

--- greetings.py
+++ greetings.py
@@ -1,12 +1,12 @@
-username = 'Ivanov'
-title = 'Zametka'
-content = 'Kupit tetrad'
-status = 'в работе'
-created_date = "10-11-2024"
-issue_date = "10-12-2024"
-print('Имя пользователя', username)
-print('Заголовок заметки', title)
-print('Описание заметки', content)
-print('Статус заметки', status)
-print('Дата создания заметки', created_date)
-print('Дата истечения заметки', issue_date)
+data_notes = {
+    'username': 'Ivanov', 'title': 'Zametka', 'content': 'Kupit tetrad',
+    "status": 'в работе', 'created_date': "10-11-2024", 'issue_date': "10-12-2024"
+}
+data_docs = (
+    'Имя пользователя', 'Заголовок заметки', 'Описание заметки',
+    'Статус заметки', 'Дата создания заметки', 'Дата истечения заметки'
+)
+
+for i, value in enumerate(data_notes.values()):
+    print(data_docs[i], value)
+
